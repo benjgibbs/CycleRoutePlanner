e17153fb0f28cce9119f608785804e48410b8d1b
Ben <benjgibbs@gmail.com>
2011-12-29 16:58:04 +0000

    Initial Commit

---
+++ CycleRoutePlanner/src/net/cyclerouteplanner/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package net.cyclerouteplanner.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
