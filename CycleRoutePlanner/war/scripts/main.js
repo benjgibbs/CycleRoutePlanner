df653a8b730be5a295d648608586cad3e7109074
Ben <benjgibbs@gmail.com>
2012-01-03 22:24:25 +0000

    Combining routes implemented. Reset implemented. Trying a div instead of a text box due to unreliable updates

--- CycleRoutePlanner/war/scripts/main.js
+++ CycleRoutePlanner/war/scripts/main.js
@@ -2,8 +2,9 @@ var map;
 var elevatorSvc;
 var directionsSvc;
 var directionsRdr;
-
-var points;
+var clicks;
+var combinedRoute;
+var distance;
 
 function initialize() {
 	var home = new google.maps.LatLng(51.0747504771771, -1.3252487182617188);
@@ -21,7 +22,7 @@ function initialize() {
 	directionsRdr = new google.maps.DirectionsRenderer();
 	directionsRdr.setMap(map);
 	google.maps.event.addListener(map, 'click', clickHandler);
-	points = [];
+	resetRoute();
 }
 
 
@@ -32,45 +33,65 @@ function log(msg) {
 }
 
 function clickHandler(event) {
-	
 	var latLng = event.latLng;
-	points.push(latLng);
-	if(points.length > 1) {
-		var start = points[0];
-		var end = points[points.length-1];
-		var waypts = [];
-		for(var idx = 1; idx < points.length-2; ++idx){
-			waypts.push({
-				location:points[idx],
-				stopover:false
-			});
-		}
-		
-		log("Calculating Route between: " + start + " and " + end + "\n");
-		
-		var request = {
+	clicks.push(latLng);
+	log("Position: " + latLng + "\n");
+	
+	if(clicks.length < 2){
+		return;
+	}
+
+	var start = clicks[clicks.length-2];
+	var end = clicks[clicks.length-1];
+	var request = {
 			origin:start,
 			destination:end,
-			waypoints:waypts,
 			//travelMode:google.maps.TravelMode.BICYCLING,
 			travelMode:google.maps.TravelMode.DRIVING,
 			unitSystem:google.maps.UnitSystem.METRIC,
 			provideRouteAlternatives:false
 		};
-		
-		directionsSvc.route(request, function(result, status){
-			if(status == google.maps.DirectionsStatus.OK){
-				directionsRdr.setDirections(result);
-				document.getElementById("distance").innerText = result.routes[0].legs[0].distance.text;
+	
+	log("Calculating Route between: " + start + " and " + end + "\n");
+	
+	directionsSvc.route(request, function(result, status){
+		if(status == google.maps.DirectionsStatus.OK){
+			var rr = result.routes[0];
+			distance += rr.legs[0].distance.value;
+			if(combinedRoute == null){
+				combinedRoute = result;
 			} else {
-				log("Failed to get route. Status: " + status + "\n");
+				var cr = combinedRoute.routes[0];
+				// Combining taken from : http://lemonharpy.wordpress.com/2011/12/15/working-around-8-waypoint-limit-in-google-maps-directions-api/#comment-34
+				cr.legs = cr.legs.concat(rr.legs);
+				cr.overview_path = cr.overview_path.concat(rr.overview_path);
+				cr.bounds = cr.bounds.extend(rr.bounds.getNorthEast());
+				cr.bounds = cr.bounds.extend(rr.bounds.getSouthWest());
 			}
-		});
-	}
+			
+			directionsRdr.setDirections(combinedRoute);
+			document.getElementById("distance").innerText = (distance/1000.0).toFixed(2) + " km";
+		} else {
+			log("Failed to get route. Status: " + status + "\n");
+		}
+	});
+}
 
-	log("Position: " + latLng + " Elevation: ");
+function resetRoute(){
+	lastPoint = null;
+	clicks = [];
+	combinedRoute = null;
+	distance = 0.0;
+	document.getElementById("distance").innerText = "0 m";
+	//directionsRdr.setDirections(null);
+}
+
 	
 	
+	
+	
+	/*
+	 * Lets do the elevations at the end...
 	var locations = [];
 	locations.push(latLng);
 	var positionalRequest = {
@@ -91,6 +112,5 @@ function clickHandler(event) {
 			alert("Elevation service failed due to: " + status);
 		}
 	});
-
+	*/
 	
-}
